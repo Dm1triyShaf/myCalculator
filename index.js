71a4bbe156cb3772a319bc16fecd3b6e02b9918e
Dmitriy Shafalyuk <97801899+Dm1triyShaf@users.noreply.github.com>
2022-08-05 18:59:18 +0300

    firstCommit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let b = ''; // second number
 let sign = ''; // знак операции
 let finish = false;
 
-const digit = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+const digit = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '.', '0'];
 const action = ['-', '+', 'X', '/'];
 
 // экран 
@@ -14,19 +14,78 @@ function clearAll() {
     b = ''; //second number
     sign = ''; //знак
     finish = false;
-    out.textContent = 0;
+    out.textContent = '0';
 }
 
 document.querySelector('.ac').onclick = clearAll;
 
 document.querySelector('.buttons').onclick = (event) => {
     //нажата не кнопка 
-    if(!event.target.classList.contains('btn')) return;
+    if (!event.target.classList.contains('btn')) return;
     //нажата кнопка clearAll ac
-    if(event.target.classList.contains('ac')) return;
+    if (event.target.classList.contains('ac')) return;
 
     out.textContent = '';
 
     //получаю нажатую кнопку
     const key = event.target.textContent
+
+    //если нажата 0-9 или .
+    if (digit.includes(key)) {
+        if (b === '' && sign === '') {
+
+            a += key;
+            console.log(a, b, sign);
+            out.textContent = a;
+        }
+
+        else if (a !== '' && b !== '' && finish) {
+            b = key;
+            finish = false;
+            out.textContent = b;
+        }
+
+        else {
+            b += key;
+            out.textContent = b;
+        }
+        console.table(a, b, sign);
+        return;
+    }
+    //если нажата + - / *
+    if (action.includes(key)) {
+        sign = key;
+        out.textContent = sign;
+        console.table(a, b, sign);
+        return;
+    }
+
+    // нажата =
+    if (key === '=') {
+        if (b === '') b=a;
+        switch(sign) {
+            case "+":
+                a= (+a) + (+b);
+                break;
+            case "-":
+                a=  a - b;
+                break;
+            case "X":
+                a= a * b;
+                break;
+            case "/":
+                if (b === '0') {
+                    out.textContent = 'error';
+                    a = '';
+                    b = '';
+                    sign = '';
+                    return;
+                }
+                a = a / b;
+                break;
+        }
+        finish = true;
+        out.textContent = a;
+        console.table(a,b,sign);
+    }
 }
